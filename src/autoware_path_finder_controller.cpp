49f837373181fd591772834a8f33e192f4905b31
caner ezeroğlu <caner.ezeroglu@isuzu.com.tr>
2021-08-26 18:14:13 +0300

    Add path line follower arrow

--- src/autoware_path_finder_controller.cpp
+++ src/autoware_path_finder_controller.cpp
@@ -7,7 +7,6 @@
 #include "std_msgs/msg/string.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "std_msgs/msg/header.hpp"
-#include "lgsvl_msgs/msg/vehicle_control_data.hpp"
 #include <cmath>
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/point.hpp"
@@ -15,6 +14,8 @@
 #include "geometry_msgs/msg/pose_stamped.h"
 #include <rclcpp/qos.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
 
 #include <vector>
 
@@ -24,6 +25,8 @@ using namespace std::chrono_literals;
 using namespace std;
 using std::placeholders::_1;
 
+int myint;
+vector<int> myvector;
  
 class PathNode : public rclcpp::Node
 {
@@ -31,30 +34,61 @@ class PathNode : public rclcpp::Node
  PathNode() : Node("path")
  {
     publisher_ = this->create_publisher<nav_msgs::msg::Path>("path_ang", 10);
+    publish_ = this->create_publisher<geometry_msgs::msg::PoseArray>("path_geo", 10);
+    publishspace_ = this ->create_publisher<geometry_msgs::msg::Quaternion>("path_quaternion", 10);
+
+    timer_ = this->create_wall_timer(100ms, std::bind(&PathNode::publishPose, this));
+    RCLCPP_INFO(this-> get_logger(), "Path Angle Has Been Started!");
  }
  
  private:
     void publishPose()
  {
-     for(int i=0;i<180;i++)
+     
+     geometry_msgs::msg::PoseArray path_geo1_;
+     nav_msgs::msg::Path path_;
+     geometry_msgs::msg::Quaternion path_quaternion1;
+     for(int theta=0;theta<180;theta++)
      {
-         poses->header.stamp.sec = 0;
-         poses->header.stamp.nanosec = 0;
-         poses->header.frame_id = "map";
-         poses->pose.position.x = 15*sin(theta);
-         poses->pose.position.y = 7.5*sin(theta++);
-         poses->pose.position.z = 0;
-         path_->poses.push_back(*poses);
+         geometry_msgs::msg::PoseStamped poses_stamped;
+         poses_stamped.header.stamp = rclcpp::Node::now();
+         poses_stamped.header.frame_id = "map";
+         poses_stamped.pose.position.x = theta;
+         poses_stamped.pose.position.y = 5*sin(0.2*theta);
+         poses_stamped.pose.position.z = 0;
+         path_.poses.push_back(poses_stamped);
+
+         geometry_msgs::msg::Pose poses;
+         path_geo1_.poses.push_back(poses); 
+         poses.position.x = theta;
+         poses.position.y = 5*sin(0.2*theta);
+         poses.position.z = 0;
+         path_geo1_.poses.push_back(poses);
+
+
+         poses.orientation. = theta;
+
+
+         
+        
      }
-     path_->header.frame_id = "map";
-     path_->header.stamp.sec = 0;
-     path_->header.stamp.nanosec = 0;
-     publisher_->publish(*path_);
+     
+     
+     path_.header.stamp = rclcpp::Node::now();
+
+     path_.header.frame_id = "map";
+     publisher_->publish(path_);
+     
+     path_geo1_.header.frame_id = "map";
+     path_geo1_.header.stamp = rclcpp::Node::now();
+     publish_->publish(path_geo1_);
+     
  }
+ 
+ rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr publish_;
+ rclcpp::TimerBase::SharedPtr timer_;
  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr publisher_;
- geometry_msgs::msg::PoseStamped::SharedPtr poses;
- nav_msgs::msg::Path::SharedPtr path_;
- int8_t theta;
+ rclcpp::Publisher<geometry_msgs::msg::Quaternion>::SharedPtr publishspace_;
 };
 
 int main(int argc, char *argv[])
